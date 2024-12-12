9cf984a4b1a01bd3c67733fec2731b0d00617878
Krayorn <nath.arki36@gmail.com>
2024-12-12 23:37:20 +0100

    structured content + started broad ideas

---
+++ src/content.config.ts
@@ -0,0 +1,33 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob } from 'astro/loaders';
+
+// 3. Define your collection(s)
+const cases = defineCollection({
+    loader: glob({ pattern: "./*.json", base: "./src/content/cases" }),
+    schema: z.object({
+        case_number: z.string(),
+        name: z.string(),
+        opened_on: z.string(),
+        closed_on: z.string(),
+        status: z.string(),
+        priority_detective: z.string()
+    }),
+});
+
+const personal = defineCollection({
+    loader: glob({ pattern: "./*.json", base: "./src/content/personal" }),
+    schema: z.object({
+        name: z.string(),
+        rank: z.string(),
+        joined_on: z.string(),
+        status: z.string(),
+        department: z.string(),
+        matriculation_number: z.string()
+    }),
+});
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { cases, personal };
